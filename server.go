b11546dc8e8cbb04c901d8b4705b2594eb0941cc
Ilia Choly <ilia.choly@gmail.com>
2015-11-18 17:14:02 -0500

    split up

--- server.go
+++ server.go
@@ -13,48 +13,6 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-// Application configuration
-type Application struct {
-	Name        string `json:"name"`
-	RedirectURL string `json:"redirect_url"`
-	WebHookURL  string `json:"webhook_url"`
-}
-
-// Applications configuration
-type Applications []*Application
-
-// Config is the configuration
-type Config struct {
-	ListenAddr     string       `json:"listen_addr"`
-	PrivateKeyFile string       `json:"private_key_file"`
-	Applications   Applications `json:"applicationds"`
-}
-
-// Lookup Applications by name
-func (a Applications) Lookup(name string) (*Application, bool) {
-	for _, app := range a {
-		if app.Name == name {
-			return app, true
-		}
-	}
-	return nil, false
-}
-
-// WebHooks gets all the hook urls
-func (a Applications) WebHooks() (hooks []string) {
-	for _, app := range a {
-		hooks = append(hooks, app.WebHookURL)
-	}
-	return
-}
-
-// User info
-type User struct {
-	ID     int64    `json:"id"`
-	Name   string   `json:"name"`
-	Groups []string `json:"groups"`
-}
-
 var (
 	signingKey *rsa.PrivateKey
 	config     Config
@@ -85,17 +43,6 @@ func init() {
 		log.Fatal(err)
 	}
 	signingKey = key
-}
-
-// JWT converts the user to a JSON Web Token
-func (u *User) JWT() (string, error) {
-	token := jwt.New(jwt.SigningMethodRS256)
-	token.Claims = map[string]interface{}{
-		"id":     u.ID,
-		"name":   u.Name,
-		"groups": u.Groups,
-	}
-	return token.SignedString(signingKey)
 }
 
 // Authenticate a user
